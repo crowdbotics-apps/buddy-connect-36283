5dc0067dfa4cdc97e56c3568e97a85929aefb619
Crowdbotics <team@crowdbotics.com>
2022-07-19 11:57:16 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "buddy_connect_36283",
+  url: "https://buddy_connect_36283.botics.co",
+  api: "https://buddy_connect_36283.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
